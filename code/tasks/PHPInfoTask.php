6b0904b2f9fa64a8d66b2bbb9a38be031b376622
spekulatius <petert@catalyst.net.nz>
2015-12-13 17:55:45 +1300

    Converted to PSR-2

--- code/tasks/PHPInfoTask.php
+++ code/tasks/PHPInfoTask.php
@@ -4,38 +4,43 @@
  *
  * Can be useful if you can't run this command directly on the server because you haven't got the shell access.
  */
-class PHPInfoTask extends BuildTask {
-	/**
-	 * title of the task
-	 *
-	 * @var string
-	 */
-	public $title = 'Display PHPInfo information';
+class PHPInfoTask extends BuildTask
+{
+    /**
+     * title of the task
+     *
+     * @var string
+     */
+    public $title = 'Display PHPInfo information';
 
-	/**
-	 * description of the task
-	 *
-	 * @var string
-	 */
-	public $description = 'Displays the output of phpinfo() for this environment.';
+    /**
+     * description of the task
+     *
+     * @var string
+     */
+    public $description = 'Displays the output of phpinfo() for this environment.';
 
-	/**
-	 * @param SS_HTTPResponse $request
-	 */
-	public function run($request = null) {
-		if (Permission::check('ADMIN') !== true) {
-			$this->message('Only admins can run this task.');
-		} else {
-			phpinfo();
-		}
-	}
+    /**
+     * @param SS_HTTPResponse $request
+     */
+    public function run($request = null)
+    {
+        if (Permission::check('ADMIN') !== true) {
+            $this->message('Only admins can run this task.');
+        } else {
+            phpinfo();
+        }
+    }
 
-	/**
-	 * @param string $text
-	 */
-	protected function message($text) {
-		if(PHP_SAPI !== 'cli') $text = '<p>' . $text.'</p>';
+    /**
+     * @param string $text
+     */
+    protected function message($text)
+    {
+        if (PHP_SAPI !== 'cli') {
+            $text = '<p>' . $text.'</p>';
+        }
 
-		echo $text . PHP_EOL;
-	}
+        echo $text . PHP_EOL;
+    }
 }
